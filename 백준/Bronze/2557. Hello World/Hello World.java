dfe0b0dc37ed6c65f484c608680b590aa0e3cf64
sihuxx <sihu714@gmail.com>
2025-12-15 15:34:21 +0900

    [Bronze V] Title: Hello World, Time: 92 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+// import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Hello World!");
+    }
+}
