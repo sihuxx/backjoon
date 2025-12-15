1d92002bef1fa5301ce07842e8cbff610e88f1e2
sihuxx <sihu714@gmail.com>
2025-12-15 15:31:13 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 224 ms, Memory: 17928 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j <= num - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
