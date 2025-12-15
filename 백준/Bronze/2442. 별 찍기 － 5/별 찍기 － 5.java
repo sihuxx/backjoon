a07068d9139508296db5d64a2841389910f3d95d
sihuxx <sihu714@gmail.com>
2025-12-15 15:31:41 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 352 ms, Memory: 20708 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for (int n=1; n <= num; n++) {
+            for (int i = num; i > n; i--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (n*2)-1; j++) {
+                System.out.print("*");
+            }
+                System.out.println("");
+        }
+    }
+}
