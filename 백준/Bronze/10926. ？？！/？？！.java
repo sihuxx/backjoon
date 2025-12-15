8057dc5b85c6d0c0b36c2080604a863d85ab7584
sihuxx <sihu714@gmail.com>
2025-12-15 15:40:13 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+        System.out.print(id + "??!");
+    }
+}
